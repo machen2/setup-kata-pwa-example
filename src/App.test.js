e2045a8486c61befecdd97ab4d7d543a898e9071
Tori Machen <tmachen@pillartechnology.com>
2018-12-28 11:41:37 -0600

    pull out wrapper into a before each block

--- src/App.test.js
+++ src/App.test.js
@@ -15,31 +15,34 @@ it('should use Enzyme', () => {
 });
 
 describe('FizzBuzz', () => {
-    // Given a user is on the Fizzbuzz form
-    // When she enters any number into an input box
-    // AND clicks the submit button
-    // Then the number is displayed
-    it('has an input field', () => {
-      const wrapper = mount(<App/>);
-      expect(wrapper.find('input').length).toEqual(1);
-    });
+  let wrapper;
 
-    it('should only display the text entered in the input when button is pressed', () => {
-        const wrapper = mount(<App/>);
-        wrapper.find('input').simulate('change', { target: { value: 18 }});
-        expect(wrapper.find('p').length).toEqual(0);
+  beforeEach(() => {
+    wrapper = mount(<App/>);
+  });
 
-        wrapper.find('button').simulate('click');
-        expect(wrapper.find('p').text()).toEqual('18');
-    });
+  // Given a user is on the Fizzbuzz form
+  // When she enters any number into an input box
+  // AND clicks the submit button
+  // Then the number is displayed
+  it('has an input field', () => {
+    expect(wrapper.find('input').length).toEqual(1);
+  });
 
-    it('should not display the text when user starts to enter a second input', () => {
-        const wrapper = mount(<App/>);
-        wrapper.find('input').simulate('change', { target: { value: 18 }});
-        wrapper.find('button').simulate('click');
-        expect(wrapper.find('p').text()).toEqual('18');
+  it('should only display the text entered in the input when button is pressed', () => {
+      wrapper.find('input').simulate('change', { target: { value: 18 }});
+      expect(wrapper.find('p').length).toEqual(0);
 
-        wrapper.find('input').simulate('change', { target: { value: 70 }});
-        expect(wrapper.find('p').length).toEqual(0);
-    });
+      wrapper.find('button').simulate('click');
+      expect(wrapper.find('p').text()).toEqual('18');
+  });
+
+  it('should not display the text when user starts to enter a second input', () => {
+      wrapper.find('input').simulate('change', { target: { value: 18 }});
+      wrapper.find('button').simulate('click');
+      expect(wrapper.find('p').text()).toEqual('18');
+
+      wrapper.find('input').simulate('change', { target: { value: 70 }});
+      expect(wrapper.find('p').length).toEqual(0);
+  });
 });
